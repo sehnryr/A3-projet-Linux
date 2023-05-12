5077ff0d807de159fe9e189b0dbdeb2f58e36d8a
sehnryr <youn@melois.dev>
2023-05-12 13:46:37 +0200

    refactorisation du code, encapsulation dans des fonctions

--- main.sh
+++ main.sh
@@ -34,6 +34,88 @@ if [ -z "$SENDER_EMAIL" ]; then
     SENDER_EMAIL=$(echo "$SMTP_LOGIN" | sed 's/%40/@/g')
 fi
 
+# Fonction d'envoi d'un mail pour notifier l'ajout d'un utilisateur
+send_mail() {
+    USER_NAME="$1"
+    USER_SURNAME="$2"
+    USER_USERNAME="$3"
+    USER_PASSWORD="$4"
+    USER_EMAIL="$5"
+
+    # Création du message
+    message="Bonjour $USER_NAME $USER_SURNAME,
+
+Votre compte a été créé sur le serveur $SERVER_IP.
+
+Voici les informations de connexion :
+Nom d'utilisateur : $USER_USERNAME
+Mot de passe : $USER_PASSWORD
+
+Le mot de passe doit être changé lors de la première connexion."
+
+    # Envoi du mail
+    ssh $SERVER_USER@$SERVER_IP -n "mail \
+        --subject \"Création de compte\" \
+        --exec \"set sendmail=smtp://$SMTP_LOGIN:$SMTP_PASSWORD@$SMTP_SERVER\" \
+        --append \"From:$SENDER_EMAIL\" \
+        $USER_EMAIL <<< \"$message\" &> /dev/null"
+}
+
+# Fonction pour ajouter une tache cron
+add_cron() {
+    crontab -l 2> /dev/null | { cat; echo "$1"; } | crontab -
+}
+
+# Fonction d'installation de Eclipse IDE for Java Developers
+eclipse_install() {
+    # Récupération de la dernière version d'Eclipse IDE for Java Developers depuis le site officiel pour Linux 64-bit
+    wget "https://www.eclipse.org/downloads/packages/" -O /tmp/eclipse.html -q
+    eclipse_url=$(grep -oP "/technology/epp/downloads/release/[^/]+/R/eclipse-java-[^/]+-R-linux-gtk-x86_64.tar.gz" /tmp/eclipse.html | head -n 1)
+    eclipse_url="https://www.eclipse.org/downloads/download.php?file=$eclipse_url&r=1"
+
+    # Téléchargement de l'archive
+    wget "$eclipse_url" -O /tmp/eclipse.tar.gz -q
+
+    # Extraction de l'archive dans /usr/local/share
+    tar -xzf /tmp/eclipse.tar.gz -C /usr/local/share
+
+    # Changement du propriétaire du répertoire
+    chown -R root:root /usr/local/share/eclipse
+
+    # Création d'un lien symbolique vers le binaire
+    ln -s /usr/local/share/eclipse/eclipse /usr/local/bin/eclipse
+
+    # Nettoyage des fichiers temporaires
+    rm /tmp/eclipse.html
+    rm /tmp/eclipse.tar.gz
+}
+
+# Fonction d'installation de Nextcloud sur le serveur distant
+nextcloud_install() {
+    # Installation de snapd si nécessaire sur la machine distante
+    ssh "$SERVER_USER@$SERVER_IP" apt install snapd -y
+
+    # Installation de Nextcloud sur la machine distante
+    ssh "$SERVER_USER@$SERVER_IP" snap install nextcloud
+
+    # Configuration de l'administrateur Nextcloud
+    ssh "$SERVER_USER@$SERVER_IP" nextcloud.manual-install "nextcloud-admin" "N3x+_Cl0uD"
+}
+
+# Fonction d'ajout d'un utilisateur à Nextcloud sur le serveur distant
+nextcloud_add_user() {
+    USER_NAME="$1"
+    USER_SURNAME="$2"
+    USER_USERNAME="$3"
+    USER_PASSWORD="$4"
+
+    # Ajout de l'utilisateur à Nextcloud sur la machine distante
+    ssh "$SERVER_USER@$SERVER_IP" OC_PASS="$USER_PASSWORD" nextcloud.occ user:add \
+        --password-from-env \
+        --display-name "$USER_NAME $USER_SURNAME" \
+        $USER_USERNAME
+}
+
 # Récupération du chemin du script
 script_path=$(dirname "$(realpath "$0")")
 
@@ -76,62 +158,12 @@ EOF
 chown root:root /home/retablir_sauvegarde
 chmod 755 /home/retablir_sauvegarde
 
-# Fonction d'installation de Eclipse IDE for Java Developers
-eclipse_install() {
-    # Récupération de la dernière version d'Eclipse IDE for Java Developers depuis le site officiel pour Linux 64-bit
-    wget "https://www.eclipse.org/downloads/packages/" -O /tmp/eclipse.html -q
-    eclipse_url=$(grep -oP "/technology/epp/downloads/release/[^/]+/R/eclipse-java-[^/]+-R-linux-gtk-x86_64.tar.gz" /tmp/eclipse.html | head -n 1)
-    eclipse_url="https://www.eclipse.org/downloads/download.php?file=$eclipse_url&r=1"
-
-    # Téléchargement de l'archive
-    wget "$eclipse_url" -O /tmp/eclipse.tar.gz -q
-
-    # Extraction de l'archive dans /usr/local/share
-    tar -xzf /tmp/eclipse.tar.gz -C /usr/local/share
-
-    # Changement du propriétaire du répertoire
-    chown -R root:root /usr/local/share/eclipse
-
-    # Création d'un lien symbolique vers le binaire
-    ln -s /usr/local/share/eclipse/eclipse /usr/local/bin/eclipse
-
-    # Nettoyage des fichiers temporaires
-    rm /tmp/eclipse.html
-    rm /tmp/eclipse.tar.gz
-}
-
 # Installation de Eclipse IDE for Java Developers
 eclipse_install
 
 # Bloquer les connexions de type FTP et toutes les connexions dans le protocole UDP
 ufw deny ftp
 ufw deny proto udp from any to any
-
-# Fonction d'installation de Nextcloud sur le serveur distant
-nextcloud_install() {
-    # Installation de snapd si nécessaire sur la machine distante
-    ssh "$SERVER_USER@$SERVER_IP" apt install snapd -y
-
-    # Installation de Nextcloud sur la machine distante
-    ssh "$SERVER_USER@$SERVER_IP" snap install nextcloud
-
-    # Configuration de l'administrateur Nextcloud
-    ssh "$SERVER_USER@$SERVER_IP" nextcloud.manual-install "nextcloud-admin" "N3x+_Cl0uD"
-}
-
-# Fonction d'ajout d'un utilisateur à Nextcloud sur le serveur distant
-nextcloud_add_user() {
-    USER_NAME="$1"
-    USER_SURNAME="$2"
-    USER_USERNAME="$3"
-    USER_PASSWORD="$4"
-
-    # Ajout de l'utilisateur à Nextcloud sur la machine distante
-    ssh "$SERVER_USER@$SERVER_IP" OC_PASS="$USER_PASSWORD" nextcloud.occ user:add \
-        --password-from-env \
-        --display-name "$USER_NAME $USER_SURNAME" \
-        $USER_USERNAME
-}
 
 # Installation de Nextcloud sur le serveur distant
 nextcloud_install
@@ -166,29 +198,14 @@ while IFS=';' read -r name surname mail password; do
 
     # Envoi d'un mail à l'adresse du destinataire avec les informations de connexion de l'utilisateur
     # nouvellement créé
-    ssh $SERVER_USER@$SERVER_IP -n \
-        "mail --subject \"Compte créé pour $name $surname\" \
-        --exec \"set sendmail=smtp://$SMTP_LOGIN:$SMTP_PASSWORD@$SMTP_SERVER\" \
-        --append \"From:$SENDER_EMAIL\" \
-        $mail <<EOF &> /dev/null
-Le compte de $name $surname a été créé.
-
-Nom d'utilisateur: $username
-Mot de passe: $password
-
-Le mot de passe devra être changé à la première connexion.
-EOF"
+    send_mail $name $surname $username $password $mail
 
     # Tout les jours de la semaine hors week-end à 23h, on compressera le 
     # répertoire "a_sauver" de l'utilisateur et on le copiera sur la machine distante
     # dans le répertoire "saves". Le fichier sera nommé "save-<utilisateur>.tgz"
     # et doit écraser le fichier précédent s'il existe.
-    crontab -l 2> /dev/null | {
-        cat;
-        echo "0 23 * * 1-5 \
-        tar -cz --directory=/home/$username/a_sauver . | \
-        SSH_AUTH_SOCK=$SSH_AUTH_SOCK ssh $SERVER_USER@$SERVER_IP 'cat > $SAVES_DIR/save-$username.tgz'";
-    } | crontab - # Ajout de la tâche dans le crontab de l'utilisateur
+    add_cron "0 23 * * 1-5 tar -cz --directory=/home/$username/a_sauver . | \
+        SSH_AUTH_SOCK=$SSH_AUTH_SOCK ssh $SERVER_USER@$SERVER_IP 'cat > $SAVES_DIR/save-$username.tgz'"
 
     # Ajout de l'utilisateur à Nextcloud sur la machine distante
     nextcloud_add_user "$name" "$surname" "$username" "$password"
